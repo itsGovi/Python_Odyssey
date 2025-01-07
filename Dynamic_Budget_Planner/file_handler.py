844771c31cab5c4c98bde1956d3984a27a31c447
itsGovi <govardhanyadavsangi@gmail.com>
2025-01-07 07:56:39 +0530

    I've finished writing file_handler.py

--- Dynamic_Budget_Planner/file_handler.py
+++ Dynamic_Budget_Planner/file_handler.py
@@ -2,6 +2,29 @@
 Manages file operations like reading and writing JSON/CSV data.
 Example: read_json(file_path) or write_json(file_path, data).
 """
+import json
+import os
+
 
 def read_json(file_path):
-    expenses = open file_path in 'r'
+    if not os.path.exists(file_path):
+        return []
+    try:
+        with open(file_path, 'r') as expenses_file:
+            expenses = json.load(expenses_file)
+            return expenses
+    except FileNotFoundError as e:
+        print(f"file not found - {e}")
+    except json.JSONDecodeError as e:
+        print(f"Invalid JSON data - {e}")
+    except Exception as e:
+        print(f"An unexpected error occured: {e}")
+        return {}
+
+def write_json(file_path, data):
+    data_dir = os.path.exists(file_path)
+    if not os.path.exists(file_path):
+        os.makedirs(data_dir, exist_ok=True)
+
+    with open(file_path, 'w') as expenses_file:
+        json.dump(data, expenses_file, indent=4)

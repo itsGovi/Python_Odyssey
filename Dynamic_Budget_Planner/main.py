2b046b2a0912a1ab8298243ddbbb711d7de6b2dd
itsGovi <govardhanyadavsangi@gmail.com>
2025-01-17 00:12:09 +0530

    starting writing code for taking input from user, as writing program for expense_handler.py started to make no sense when there's no variable at main.py to consider, small fuck-ups lead to big UNKNOWN errors!

--- Dynamic_Budget_Planner/main.py
+++ Dynamic_Budget_Planner/main.py
@@ -2,16 +2,18 @@
 This is the program's "brain." It handles user input, menu navigation, and calls functions from other files.
 Example: Display menu options like "Add Expense" or "View Expenses" and call the respective function from `expense_handler.py`.
 """
-
-from file_handler import read_json, write_json
-
-file_path = 'Dynamic_Budget_Planner/data/expenses(test_v1).json'
-
-# test data
-expenses = [
-    {"amount": 500, "date": "2024-12-22", "category": "Food", "description": "Lunch"}
-]
-write_json(file_path, expenses)
-
-data = read_json(file_path)
-print(f"Expenses Data:\n {data}")
+while True:
+    print("\nHello, what are you here for?")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Update Expense")
+    print("4. Delete Expense")
+    
+    try:
+        choice = int(input("Enter your choice(1-4)"))
+        if 1 <= choice <= 4:
+            break #valid input exitsing the loop
+        else:
+            print("Invalid choice. Please enter a number between 1 and 4.")
+    except ValueError:
+        print("Invalid input. Please enter a number!")
